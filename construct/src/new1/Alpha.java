7525bc5f66809861944eda167eac8508c3bf2b7f
KayranA <101198706+KayranA@users.noreply.github.com>
2022-03-15 15:16:21 +0530

    Update Alpha.java
    Updated program

--- construct/src/new1/Alpha.java
+++ construct/src/new1/Alpha.java
@@ -4,5 +4,6 @@ public class Alpha extends Demo{
       static void demo()
       {
     	  System.out.println("I am Alpha");
+            System.out.println("I am Practicing on GitHub");
       }
 }
