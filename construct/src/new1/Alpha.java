1849e98a9083e8d04d977e686c6e64ccaa9fdebb
KayranA <karanrathod1604@gmail.com>
2022-04-30 20:40:04 +0530

    Made some Changes with other 2 Noobs

--- construct/src/new1/Alpha.java
+++ construct/src/new1/Alpha.java
@@ -3,7 +3,7 @@ package new1;
 public class Alpha extends Demo{
       static void demo()
       {
-    	  System.out.println("I am Alpha");
+    	  System.out.println("I am Noob");
             System.out.println("I am Practicing on GitHub");
       }
 }
